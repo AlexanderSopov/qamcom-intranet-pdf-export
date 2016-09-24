cd0cee0ff975502f3e075cc55db1604be1218151
Alexander Sopov <sopovalexander@gmail.com>
2016-09-24 22:46:57 +0200

    automated command run, make sure to make current working dir more platform independent in watcher.js

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ##install:
 1. install java jdk
 2. cd into repository root folder
-3. run command: java -cp scroll-pdf-3.4.5.jar org.xhtmlrenderer.simple.PDFRenderer export/CLEANED_32552835.xhtml test.pdf
+3. run command: java -cp scroll-pdf-3.4.5.jar org.xhtmlrenderer.simple.PDFRenderer export/CLEANED_32539763.xhtml test.pdf
 4. exported pdf is at test.pdf
---
+++ watcher.js
@@ -0,0 +1,15 @@
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) 
+		console.log('exec error: ' + error);
+} 
+exec(
+	"java -cp scroll-pdf-3.4.5.jar "+
+	"org.xhtmlrenderer.simple.PDFRenderer "+
+	"export/CLEANED_32539763.xhtml "+
+	"test.pdf", {
+		cwd: "H:/TeddyBear/Qamcom/qamcom-intranet-pdf-export/workingDir/"
+	}, puts);
+//
