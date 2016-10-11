456750a2964abf5a227c6179cfe3f55cface50f6
sopovalexander@gmail.com <sopovalexander@gmail.com>
2016-10-11 15:47:27 +0200

    latest version, almost fully functional

--- watcher.js
+++ watcher.js
@@ -1,14 +1,14 @@
 var exec 	= require('child_process').exec
 function puts(error, stdout, stderr) {
+	if (error)
+		return console.log('exec error: ' + error);
 	console.log('stdout: ' + stdout);
 	console.log('stderr: ' + stderr);
-	if (error !== null) 
-		console.log('exec error: ' + error);
-} 
+}
 exec(
 	"java -cp scroll-pdf-3.4.5.jar "+
 	"org.xhtmlrenderer.simple.PDFRenderer "+
-	"export/CLEANED_32539763.xhtml "+
+	"export/CLEANED_32554489.xhtml "+
 	"test.pdf", {
 		cwd: process.cwd() + "/workingDir/"
 	}, puts);
